ddda53d47b65f854a2c6ca2c3e39fd46b6469253
Jorge Acero <julianacero98@gmail.com>
2019-03-12 11:58:46 -0600

    deploy

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/raee';
+const URI = 'mongodb+srv://jorge:jjalcs5x17798@cluster0-msfrl.mongodb.net/test?retryWrites=true';
 mongoose.connect(URI, { useNewUrlParser: true })
         .then(db => console.log('DB Connected'))
         .catch(err => console.error(err));
